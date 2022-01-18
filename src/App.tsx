decde8b60bc2ef7f6d96b718f8da962c5a53cfb0
Brotherbond <ehinmitankehinde@gmail.com>
2022-01-18 21:53:08 +0100

    styles and images added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./styles/global.scss"
+
 
 function App() {
+  const Home = () => {
+    return (
+      <>
+      </>
+    )
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route index element={<Home />} />
+        {/* <Route path="/connect" element={<Connect />} />
+        <Route path="/wallet" element={<Wallet />} /> */}
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/images/index.tsx
@@ -0,0 +1,11 @@
+import Logo from './logo.jpg';
+import luffy from './luffy.jpg';
+import deku from './deku.jpg';
+import onealliance from './onealliance.jpg';
+import goku from "./goku.svg";
+import chopper from "./chopper.jpg"
+
+const Images = {
+   Logo, luffy, onealliance, goku, deku, chopper
+}
+export default Images
