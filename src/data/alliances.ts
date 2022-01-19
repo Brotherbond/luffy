602e8166a35c2d39d67cfddcff686c3d3823d523
Brotherbond <ehinmitankehinde@gmail.com>
2022-01-19 08:11:21 +0100

    data folder added, includes the alliances and colouroptions

---
+++ src/data/alliances.ts
@@ -0,0 +1,26 @@
+import Images from "../images";
+
+const Alliances = [{
+    img: Images.luffy,
+    name: "Luffy",
+    unit: '1,232,232,268'
+},
+{
+    img: Images.goku,
+    name: "Goku",
+    unit: '7,452,323'
+},
+{
+    img: Images.deku,
+    name: "Deku",
+    unit: '1,232,232,268'
+},
+{
+    img: Images.chopper,
+    name: "Chopper",
+    unit: '67,232,268'
+}
+
+]
+
+export default Alliances
